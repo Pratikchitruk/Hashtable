fbba8e8d0cca6bd969022e4ed36acd22109958c4
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-15 13:58:32 +0530

    frequency.cs push for HashTable

---
+++ frequency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHashTable
+{
+    public class Frequency
+    {
+        public static void FindIndexes()
+        {
+            string searchableString = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string keyword = "paranoid";
+
+            int index = searchableString.IndexOf(keyword);
+            while (index >= 0)
+            { 
+            Console.WriteLine("Index " + index);
+                index = searchableString.IndexOf(keyword, index+keyword.Length);
+            }
+
+        }
+        
+
+    }
+}
